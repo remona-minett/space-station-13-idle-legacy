08fd4c517c94b1fc7711c0fe07e8c5835c0fd8d9
remona-minett <50187357+remona-minett@users.noreply.github.com>
2020-08-09 00:24:04 -0700

    Update deployGit.sh

--- deployGit.sh
+++ deployGit.sh
@@ -16,6 +16,6 @@ git init
 git add -A
 git commit -m 'deploy'
 
-git push -f https://github.com/remona-minett/space-station-13-idle.git master:gh-pages
+git push -f https://github.com/remona-minett/space-station-13-idle-legacy.git master:gh-pages
 
 cd -
